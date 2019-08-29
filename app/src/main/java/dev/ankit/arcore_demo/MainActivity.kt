9f53f8bfc07b18a43acf82128f9601532305d3e8
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-08-29 15:26:52 +0800

    Initial commit

---
+++ app/src/main/java/dev/ankit/arcore_demo/MainActivity.kt
@@ -0,0 +1,31 @@
+package dev.ankit.arcore_demo
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+
+    private lateinit var scoreboard: ScoreboardView
+
+    private var gameHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+
+    }
+
+    private fun failHit() {
+        scoreboard.score -= 50
+        scoreboard.life -= 1
+        if (scoreboard.life <= 0) {
+            // Game over
+            gameHandler.removeCallbacksAndMessages(null)
+        }
+    }
+}
