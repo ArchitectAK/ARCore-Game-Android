98b5f692368fd55b641bffd3a16a1b92eb4f896d
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-08-30 11:34:13 +0800

    ar enabled and working now

---
+++ app/src/main/java/dev/ankit/arcore_demo/Configuration.kt
@@ -0,0 +1,21 @@
+package dev.ankit.arcore_demo
+
+object Configuration {
+    const val COL_NUM = 3
+    const val ROW_NUM = 4
+
+    // Min / max amount of time (in ms) between
+    // two different droid moves
+    const val MIN_MOVE_DELAY_MS = 400L
+    const val MAX_MOVE_DELAY_MS = 4000L
+
+    // min / max amount of time (in ms) between
+    // pull up and pull down of the same droid
+    const val MIN_PULL_DOWN_DELAY_MS = 400L
+    const val MAX_PULL_DOWN_DELAY_MS = 2000L
+
+    // Number of moves per time
+    const val MOVES_PER_TIME = 3
+
+    const val START_LIVES = 3
+}
